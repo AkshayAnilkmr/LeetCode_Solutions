cfab7721bc8780f0cd76856d9c6c65fd7bd5c1a5
Akshay Anilkumar <121080122+AkshayAnilkmr@users.noreply.github.com>
2025-04-18 16:21:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (70.04%) - LeetSync

---
+++ 290-word-pattern/word-pattern.cpp
@@ -0,0 +1,39 @@
+
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        istringstream iss(s);
+        vector<string> words;
+        string word;
+        while (iss >> word) {
+            words.push_back(word);
+        }
+
+        if (words.size() != pattern.size()) {
+            return false;
+        }
+
+        unordered_map<char, string> char_to_word;
+        unordered_map<string, char> word_to_char;
+
+        for (int i = 0; i < pattern.size(); ++i) {
+            char c = pattern[i];
+            string w = words[i];
+
+            if (char_to_word.count(c)) {
+                if (char_to_word[c] != w) return false;
+            } else {
+                char_to_word[c] = w;
+            }
+
+            if (word_to_char.count(w)) {
+                if (word_to_char[w] != c) return false;
+            } else {
+                word_to_char[w] = c;
+            }
+        }
+
+        return true;
+    }
+};
