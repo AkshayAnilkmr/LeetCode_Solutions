aa5078f638a8f6e927a9e1cc4a0517c5bbe913ed
Akshay Anilkumar <121080122+AkshayAnilkmr@users.noreply.github.com>
2025-08-16 08:13:00 +0530

    Time: 165 ms (13.11%) | Memory: 102.4 MB (7.12%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -3,8 +3,10 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         map<int,int>mp1;
         for(int i=0;i<nums.size();i++){
-            if(mp1.find(nums[i])!=mp1.end()&&abs(i-mp1[nums[i]])<=k){
-                return true;
+            if(mp1.find(nums[i])!=mp1.end()){
+                if(abs(mp1[nums[i]]-i)<=k){
+                    return true;
+                }
             }
             mp1[nums[i]]=i;
         }
